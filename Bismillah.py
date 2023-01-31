fec54ace80ba4c28cfc1b2da0b5bd51bf1f83ac7
Bexruz <123628979+bexrooz@users.noreply.github.com>
2023-01-31 08:54:11 -0500

    Create Bismillah.py

---
+++ Bismillah.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+"""
+Bismillahir Rohmanir Rohiym
+
+coder - Bekhruz
+"""
+
+# ism = input("Ismingiz nima?")
+# print("Assalom alaykum, " + ism)
+# names = ['Ali', 'Vali', 'Hassan', 'Hussan', 'Alim']
+# for name in names:
+    # print(f'Assalamu alaykum, {name}')
+# print('Code was repeated 5 times')
+
+# numbers = list(range(11, 100, 2))
+# for number in numbers:
+#     print(f"{number}\' cube is {number**3}")
+
+# movies = []
+# print("What are 5 movies you resently watched?")
+# for movie in range(5):
+#     movies.append(input(f"{movie+1}-enter movies you watched"))
+# print(movies)
+
+# friends = []
+# print("How many friends did you meet today?")
+# for friend in range(5):
+#     friends.append(input(f'{friend+1}-enter name of your friend-'))
+# print(friends)
+
+# cars = ['audi', 'bmw', 'volvo', 'kia', 'hyundai']
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#     if car =='GM':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+# name = input("What is your name?\n>>>")
+# if name.lower() == "admin":
+#     print("Welcome, would you like to have look to a list of users?")
+# else:
+#     print(f'Welcome, {name.title()}')
+
+# password = int(input("Please enter password >>>"))
+# if password == 12345:
+#     print("Welcome to hackers\' world")
+# else:
+#     print("You hvae enrtered wrong password. Please try again!")
+
+# age = int(input("What is your age?\n>>>"))
+# if age>=18:
+#     print("welcome")
+# else:
+#     print("Get the hell out of here, you little bitch!!!")
+
+# login = input("Please choose new login!\n>>>")
+# if len(login)>=5:
+#     print("your login is good to use")
+# else:
+#     print("Please use at lease 5 letters for your login!")
+
+# birth_date = int(input("Please enter your year of birth!\n>>>"))
+# if 2023-birth_date<18:
+#     print(f"You are at {2023-birth_date}.")
+#     print("Acess not allowed. Get the fuck out of here!!!")
+# else:
+#     print("Welcome, you dirty bitch!!!")
+
+# x = float(input("enter 1st number"))
+# y = float(input("enter 2nd number"))
+# if x==y: print(f"your numbers are equal: {x}={y}")
+
+# numbers = float(input("enter any number>>>"))
+# if numbers>0: print('your number is positive') 
+# else: print('your number is negaative')
+
+# x = float(input('enter a number>>>'))
+# if x>0: print(f"the square root of {x} is {x**1/2}")
+# else: print('please enter a positive number debil blya!!!')
+
+# answer = input("What is your religon?\n>>>")
+# if answer.lower() == "muslim":
+#     print("Assalamu alaykum!!!")
+# else:
+#     print("Hello!")
+
+# one = 1
+# two = 2
+# hello = 'hello'
+# print(one + two + hello)
+# change this code
+# mystring = "hello"
+# myfloat = 10.0
+# myint = 20
+
+# # testing code
+# if mystring == "hello":
+#     print("String: %s" % mystring)
+# if isinstance(myfloat, float) and myfloat == 10.0:
+#     print("Float: %f" % myfloat)
+# if isinstance(myint, int) and myint == 20:
+#     print("Integer: %d" % myint)
+
+# x = float(input('please enter any number'))
+# if x>=1:
+#     print('the number you entered is positive')
+# # or x==0: print('your number equals to zero')
+# else: print('the number you entered is negative')
+
+# number = float(input("Please enter an even number>>>"))
+# if number%2:
+#     print('You have entered odd number')
+# else: print("Thank you!")
+
+# age = float(input("What is your age?>>>"))
+# if age<=4 or age>=60:
+#     print("Enterance is free for you!")
+# elif age<18 and age>4:
+#     print("Enterance fee is 10000 so'm for you!")
+# elif age>=18 and age<60:
+#     print("Enterance is 20000 so'm for you!")
+
+# x = float(input("Please enter first number>>>"))
+# y = float(input("Please enter 2nd number>>>"))
+
+# if x==y: print(f"your numbers are equal, {x} = {y}")
+# elif x>y: print(f"{x} is bigger than {y}")
+# else: print(f"{y} is bigger than {x}")
+
+# products = ['pen', 'book', 'eraser', 'computer', 'mouse', 'keyboard', 'flour', 'shelf', 'cake', 'bread']
+# cart = []
+# for n in range(5):
+#     cart.append(input(f"Please add {n+1} products to cart >>>"))
+# for product in cart:
+#     if product.lower() in products:
+#         print(f"We have {product.title()} in the store")
+#     else: print(f'we are out of {product.title()} for now.')
+
+# logins = ['shodi', 'bodi', 'qo\'zi', 'ali', 'vali']
+# username = input('Please choose a username:\n>>>')
+# if username.lower() in logins: print(f"please choose another username {username} is not available")
+# else:
+#     print(f"Welcome {username.title()}")
+
+# products = ['apple', 'bread', 'milk', 'egg', 'key', 'cash', 'shelf', 'mug', 'town', 'run']
+# cart = []
+# for n in range(5):
+#     cart.append(input(f"please add {n+1} st items to cart>>>"))
+# available = []
+# unavailable = []
+# for product in cart:
+#     if product in products:
+#         available.append(product)
+#     else: unavailable.append(product)
+# if unavailable:
+#     print(f'we dont have {unavailable} in our store')
+# else: print('your purchase is approved')
+
+# number = int(input("enter any number you like >>>"))
+
+# for n in range(2,11):
+#     if not (number%n):
+#         print(f'{number} number is devided to {n} perfectly')
+# if number <=1: print("please enter a number higher than 1")
+ 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
